3c8652bea6f494932d39de3d7bee4c457fa54018
ankithchowdary <ankithc29@gmail.com>
2024-10-01 22:47:17 +0530

    Update

---
+++ tuples_lists_set.py
@@ -0,0 +1,23 @@
+# Tuples: immutable, ordered, allows duplicate elements  no insert, remove, pop, clear
+t=(1,2,3,4,5);
+# Lists: mutable, ordered, allows duplicate elements
+l=[1,2,3,4,5];
+# Sets: mutable, unordered, no duplicate elements
+s={1,2,3,4,5};
+t.append(6);
+print(t);
+l.append(6);
+print(l);
+l.remove(6);
+l.reverse();
+print(l);
+l.insert(1,6);
+print(l);
+l.pop();
+print(l);
+l.clear();
+print(l);
+s.add(6);
+print(s);
+s.remove(6);
+print(s);
