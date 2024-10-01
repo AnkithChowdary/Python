fb921693d3b58b7892d8e769356060991e34eca5
ankithchowdary <ankithc29@gmail.com>
2024-10-01 22:30:10 +0530

    Update

---
+++ gettingUserInput.py
@@ -0,0 +1,9 @@
+length=int(input("Enter length: "))
+breadth=int(input("Enter breadth: "));
+area=length*breadth;
+print(f"Area of rectangle with length : {length} and breadth : {breadth} is: ",area);
+# with some precision
+length=41.66;
+breadth=10.5;
+area=length*breadth;
+print(f"Area of rectangle with length : {length:.2f} and breadth : {breadth:.2f} is: ",area);
