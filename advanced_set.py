0d46093ade23ca2a488fc317f80a826e2398fca9
Your Name <ankithchowdary25@gmail.com>
2025-05-22 08:42:27 +0530

    Update

--- advanced_set.py
+++ advanced_set.py
@@ -4,3 +4,11 @@ abroad={"Bob", "Anne"}
 local_friends=abroad.difference(friends)
 
 print(local_friends)
+
+
+all_friend=abroad.union(friends);
+print(all_friend)
+
+
+common_friends=abroad.intersection(friends)
+print(common_friends)
