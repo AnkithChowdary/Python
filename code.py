30bfbfc113cf199446bfaac0259d25f1d9011cbd
Your Name <ankithchowdary25@gmail.com>
2025-06-06 15:52:04 +0530

    Implement Book and BookShelf classes with string representation and add Flask routes for store management

---
+++ code.py
@@ -0,0 +1,5 @@
+from mymodule import divide
+
+result = divide(10, 2)
+print("Result:", result)
+ 
---
+++ typeHinting.py
@@ -0,0 +1,4 @@
+class Book:
+    def __init__(self,name:str,page_count:int):
+        self.name=name
+        self.page_count=page_count
